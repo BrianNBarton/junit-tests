832e2da7390315644fa3f4481dba4dc96b8fa4ac
Brian Barton <brian.n.barton@gmail.com>
2020-09-01 12:50:10 -0600

    tests

---
+++ src/main/Java/Student.java
@@ -0,0 +1,14 @@
+public class Student {
+}
+//    id should be a long number used to represent a "unique user" in our application.
+//        name is a String that holds the name of the student.
+//        grades is an ArrayList that contains a list of Integer numbers.
+
+
+//
+//    Create a new branch called students-tests and read carefully the next instructions.
+//
+//        Create a StudentTest.java class file inside of src/test/java (you might have to create these folders yourself) and remember to write the test before the actual production code. We will simulate the C(reate) R(ead) from the CRUD functionality in our grades application, you should be able to test and create the following requirements:
+//
+//        The Student class should have a constructor that sets both the name and id property, it initializes the grades property as an empty ArrayList.
+//        The Student class should have the following methods:
