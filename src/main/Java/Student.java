afc12e3b1573fce1231d0e94d9e5bd7f2c6421aa
Brian Barton <brian.n.barton@gmail.com>
2020-09-02 09:51:01 -0600

    unit testing

--- src/main/Java/Student.java
+++ src/main/Java/Student.java
@@ -1,14 +1,38 @@
+import java.util.ArrayList;
+
 public class Student {
+
+    private long id;
+    private String name;
+    private ArrayList<Integer> grades;
+
+    public Student(long id, String name) {
+        this.id = id;
+        this.name = name;
+        this.grades = new ArrayList<>();
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public ArrayList<Integer> getGrades() {
+        return this.grades;
+    }
+
+    public void addGrade(int grade) {
+        this.grades.add(grade);
+    }
+
+    public double getGradeAverage() {
+        double sum = 0;
+        for (int grade: this.grades) {
+            sum += grade;
+        }
+        return sum / this.grades.size() ;
+    }
 }
-//    id should be a long number used to represent a "unique user" in our application.
-//        name is a String that holds the name of the student.
-//        grades is an ArrayList that contains a list of Integer numbers.
-
-
-//
-//    Create a new branch called students-tests and read carefully the next instructions.
-//
-//        Create a StudentTest.java class file inside of src/test/java (you might have to create these folders yourself) and remember to write the test before the actual production code. We will simulate the C(reate) R(ead) from the CRUD functionality in our grades application, you should be able to test and create the following requirements:
-//
-//        The Student class should have a constructor that sets both the name and id property, it initializes the grades property as an empty ArrayList.
-//        The Student class should have the following methods:
